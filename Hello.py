20043c86fd380d512a264ec2a15f1a4d1f8164b2
baharpbzd <50031514+baharpbzd@users.noreply.github.com>
2024-01-20 19:26:24 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -22,3 +22,11 @@ if button3:
     st.write(animal)
     if animal == "Lion":
         st.write("ROAR!")
+
+st.header("Start of the Selectbox Section")
+animal2 = st.selectbox("What animal is your favorite?", ("Lion","Tiger","Bear"))
+button4 = st.button("Submit Animal 2")
+if button4: 
+    st.write(animal2)
+    if animal == "Lion":
+        st.write("ROAR!")
